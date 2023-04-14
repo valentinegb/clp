526e2a42d5ae491e61d197f193b3dfbbfd53e926
Valentine <35977727+valentinegb@users.noreply.github.com>
2023-04-13 20:00:17 -0700

    Add more documentation

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # clp
 
 Simple library for creating "command line presentations".
+
+Presentations are composed with the [`slide`](https://docs.rs/clp/latest/clp/macro.slide.html) macro, like so:
+
+```rust
+use clp::crossterm::style::{Print, Stylize};
+use clp::{slide, TypewriterPrint, TypewriterPrintStyledContent};
+use std::time::Duration;
+
+slide!(
+    TypewriterPrint("Welcome to my presentation on ", Duration::from_millis(25)),
+    TypewriterPrintStyledContent(
+        "command line presentations".bold(),
+        Duration::from_millis(50),
+    ),
+    Print("."),
+)
+.expect("the first slide should appear");
+
+slide!(TypewriterPrint(
+    "...there isn't much content on these slides.",
+    Duration::from_millis(25),
+))
+.expect("the second slide should appear");
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 //! Simple library for creating "command line presentations".
+//!
+//! Presentations are composed with the [`slide`] macro, like so:
+//!
+//! ```no_run
+//! use clp::crossterm::style::{Print, Stylize};
+//! use clp::{slide, TypewriterPrint, TypewriterPrintStyledContent};
+//! use std::time::Duration;
+//!
+//! slide!(
+//!     TypewriterPrint("Welcome to my presentation on ", Duration::from_millis(25)),
+//!     TypewriterPrintStyledContent(
+//!         "command line presentations".bold(),
+//!         Duration::from_millis(50),
+//!     ),
+//!     Print("."),
+//! )
+//! .expect("the first slide should appear");
+//!
+//! slide!(TypewriterPrint(
+//!     "...there isn't much content on these slides.",
+//!     Duration::from_millis(25),
+//! ))
+//! .expect("the second slide should appear");
+//! ```
 
 #![warn(missing_docs)]
 
